9a780c330788cf2875477f79ff0519c0bab14fb7
Lucas Mouette <mouette@hm.edu>
2024-11-21 15:50:23 +0100

    Setup express server, project and install modules + packages

---
+++ Backend/src/app.ts
@@ -0,0 +1,7 @@
+// Code written by Lucas Mouette
+
+import express, { Express, Response, Request } from 'express';
+
+const app: Express = express();
+
+export default app;
---
+++ Backend/src/server.ts
@@ -0,0 +1,12 @@
+// Code written by Lucas Mouette
+
+import dotenv from 'dotenv';
+import app from './app';
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
